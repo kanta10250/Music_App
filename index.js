b0ce5e24cd34b355ce9d48e2584b838b505d8455
kanta10250 <infrastructurekanta@gmail.com>
2025-02-13 17:36:15 +0900

    Update: visual_animation

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 window.addEventListener('load', ()  => {
     const sounds = document.querySelectorAll('.sound');
     const pads = document.querySelectorAll('.pads div');
-
-    console.log('て', sounds);
+    const visual  = document.querySelector('.visual');
+    const colors= [
+        "#60d394",
+        "#d36060",
+        "#c060d3",
+        "#d3d160",
+        "#6860d3",
+        "#60b2d3"
+    ];
 
     //Lets get going with trh Sound
     pads.forEach((pad, index) => {
         pad.addEventListener('click', function() {
+            sounds[index].currentTime= 0;
             sounds[index].play();
+
+             createBubble(index);
         });
     });
+    //Create a function that makes bubbles
+    const createBubble = (index) => {
+        const bubbles  = document.createElement("div");
+        visual.appendChild(bubbles);
+        bubbles.style.backgroundColor = colors[index];
+        bubbles.style.animation = 'jump 1s ease';
+        bubbles.addEventListener('animationend', function() {
+            visual.removeChild(this);
+        });
+    };
 });
